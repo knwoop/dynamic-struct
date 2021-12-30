e02f62ff518768368e02b05e9244b82027b86352
kenta takahashi <kenta.takahashi@showcase-gig.com>
2021-12-31 07:48:25 +0900

    tmp

--- builder.go
+++ builder.go
@@ -2,77 +2,27 @@ package dynamicstruct
 
 import "reflect"
 
-type (
-	// Builder build dynamic struct
-	Builder interface {
-		// AddField adds new struct's field.
-		AddField(name string, typ interface{}, tag string) Builder
+// Builder builds struct dynamically
+type Builder struct {
+	fields map[string]*Field
+}
 
-		// RemoveField removes existing struct's field.
-		// builder.RemoveField("SomeFloatField")
-		RemoveField(name string) Builder
-
-		// HasField checks if struct has a field with a given name.
-		HasField(name string) bool
-
-		// GetField returns struct's field definition.
-		// field := builder.GetField("SomeFloatField")
-		GetField(name string) Field
-
-		// Build returns dynamic struct.
-		Build() DynamicStruct
-	}
-
-	// FieldConfig holds single field's definition.
-	// It provides possibility to edit field's type and tag.
-	Field interface {
-		// SetType changes field's type.
-		// Expected value is an instance of golang type.
-		//
-		// field.SetType([]int{})
-		//
-		SetType(typ interface{}) Field
-		// SetTag changes fields's tag.
-		// Expected value is an string which represents classical
-		// golang tag.
-		//
-		// field.SetTag(`yaml:"slice"`)
-		//
-		SetTag(tag string) Field
-	}
-
-	// DynamicStruct contains defined dynamic struct.
-	DynamicStruct interface {
-		// New provides new instance of defined dynamic struct.
-		New() interface{}
-	}
-
-	builderImpl struct {
-		fields map[string]*fieldConfigImpl
-	}
-
-	fieldConfigImpl struct {
-		typ interface{}
-		tag string
-	}
-
-	dynamicStructImpl struct {
-		definition reflect.Type
-	}
-)
+// DynamicStruct represents a struct created dynamically by Builder
+type DynamicStruct struct {
+	definition reflect.Type
+}
 
 // NewStruct returns new clean instance of Builder interface
-//
 // builder := dynamicstruct.NewStruct()
-//
-func NewStruct() Builder {
-	return &builderImpl{
-		fields: map[string]*fieldConfigImpl{},
+func NewStruct() *Builder {
+	return &Builder{
+		fields: map[string]*Field{},
 	}
 }
 
-func (b *builderImpl) AddField(name string, typ interface{}, tag string) Builder {
-	b.fields[name] = &fieldConfigImpl{
+// AddField adds
+func (b *Builder) AddField(name string, typ interface{}, tag string) *Builder {
+	b.fields[name] = &Field{
 		typ: typ,
 		tag: tag,
 	}
@@ -80,27 +30,27 @@ func (b *builderImpl) AddField(name string, typ interface{}, tag string) Builder
 	return b
 }
 
-func (b *builderImpl) RemoveField(name string) Builder {
+func (b *Builder) RemoveField(name string) *Builder {
 	delete(b.fields, name)
 
 	return b
 }
 
-func (b *builderImpl) HasField(name string) bool {
+func (b *Builder) HasField(name string) bool {
 	_, ok := b.fields[name]
 	return ok
 }
 
-func (b *builderImpl) GetField(name string) Field {
+func (b *Builder) GetField(name string) *Field {
 	if !b.HasField(name) {
 		return nil
 	}
 	return b.fields[name]
 }
 
-func (b *builderImpl) Build() DynamicStruct {
-	var structFields []reflect.StructField
-
+// Build returns a dynamically created struct
+func (b *Builder) Build() *DynamicStruct {
+	structFields := make([]reflect.StructField, len(b.fields))
 	for name, field := range b.fields {
 		structFields = append(structFields, reflect.StructField{
 			Name: name,
@@ -109,17 +59,17 @@ func (b *builderImpl) Build() DynamicStruct {
 		})
 	}
 
-	return &dynamicStructImpl{
+	return &DynamicStruct{
 		definition: reflect.StructOf(structFields),
 	}
 }
 
-func (f *fieldConfigImpl) SetType(typ interface{}) Field {
+func (f *Field) SetType(typ interface{}) *Field {
 	f.typ = typ
 	return f
 }
 
-func (f *fieldConfigImpl) SetTag(tag string) Field {
+func (f *Field) SetTag(tag string) *Field {
 	f.tag = tag
 	return f
 }
